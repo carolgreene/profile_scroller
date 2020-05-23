c0e82520f85a01ab8988546f9182c100393d82f7
carolgreene <carolgreene3@gmail.com>
2020-05-23 06:08:43 -0700

    adjusted data & completed iterator

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const data = [
     gender: 'male',
     lookingfor: 'female',
     location: 'Boston MA',
-    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+    image: 'https://randomuser.me/api/portraits/men/76.jpg'
   },
 
   {
@@ -26,3 +26,48 @@ const data = [
     image: 'https://randomuser.me/api/portraits/men/83.jpg'
   },
 ]
+
+const profiles = profileIterator(data)
+
+//call first profile so it shows right away
+nextProfile()
+
+//Next Event
+document.getElementById('next').addEventListener('click', nextProfile);
+
+//Next Profile Display
+function nextProfile() {
+  const currentProfile = profiles.next().value;
+
+  if(currentProfile !== undefined) {
+    document.getElementById('profileDisplay').innerHTML = `
+      <ul class="list-group">
+        <li class="list-group-item">Name: ${currentProfile.name}</li>
+        <li class="list-group-item">Age: ${currentProfile.age}</li>
+        <li class="list-group-item">Location: ${currentProfile.location}</li>
+        <li class="list-group-item">Preference: ${currentProfile.gender} 
+        looking for ${currentProfile.lookingfor}</li>
+      </ul>
+      `;
+  document.getElementById('imageDisplay').innerHTML = `<img
+  src= ${currentProfile.image}>
+  `
+  } else {
+    //no more profiles
+    window.location.reload()
+  }
+}
+
+//Profile Iterator
+
+function profileIterator(profiles) {
+  let nextIndex = 0;
+
+  return {
+    next: function() {
+      return nextIndex < profiles.length ?
+      { value: profiles[nextIndex++], done: false } :
+      { done: true }
+    }
+  }
+}
