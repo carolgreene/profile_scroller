48292b9e09823b859b575a09bf5832a7834cbcb7
carolgreene <carolgreene3@gmail.com>
2020-05-23 05:36:05 -0700

    made data

---
+++ app.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    name: 'John Doe',
+    age: 32,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Boston MA',
+    image: 'https://randomuser.me/api/portraits/men/82.jpg'
+  },
+
+  {
+    name: 'Jen Smith',
+    age: 26,
+    gender: 'female',
+    lookingfor: 'male',
+    location: 'Miami FL',
+    image: 'https://randomuser.me/api/portraits/women/82.jpg'
+  },
+
+  {
+    name: 'William Johnson',
+    age: 38,
+    gender: 'male',
+    lookingfor: 'female',
+    location: 'Chicago IL',
+    image: 'https://randomuser.me/api/portraits/men/83.jpg'
+  },
+]
